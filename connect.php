165207799ce1841fe57fde425172b3b3f8ca9a21
hingeriina <hingeroostesriina@gmail.com>
2020-02-11 14:20:53 +0200

    updated username and password

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 function connect_db() {
   $servername = "localhost";
   // EDIT HERE YOUR USERNAME AND PASSWORD
-  $username = "cards";
-  $password = "password";
-  $dbname = "cards";
+  $username = "bgg307";
+  $password = "puKUoP63m";
+  $dbname = "bgg307";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); // Check connection
 if ($conn->connect_error) {
